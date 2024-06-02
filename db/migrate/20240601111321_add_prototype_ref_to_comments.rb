22015c3318c816999518b074303c703a8be89f98
necori <topodebtk7@yahoo.co.jp>
2024-06-02 17:33:01 +0900

    migrate修正

--- db/migrate/20240601111321_add_prototype_ref_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddPrototypeRefToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :comments, :prototype, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_01_133809) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_01_110436) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,12 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_01_133809) do
   end
 
   create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "prototype_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "prototype_id", null: false
-    t.text "content"
-    t.integer "user_id"
     t.index ["prototype_id"], name: "index_comments_on_prototype_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
@@ -77,5 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_01_133809) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
   add_foreign_key "prototypes", "users"
 end
